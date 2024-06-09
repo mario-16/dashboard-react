81daf781f2d25a473ce32378b42d4ad3921f76b4
mario-16 <mario_c_081293@hotmail.com>
2024-06-09 12:24:20 -0300

    Acomodo de cards debajo del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Boton } from './components/Boton'
 import { Card } from './components/Card'
 import { Navbar } from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Card/>
+          </div>
+          <div className="col">
+            <Card/>
+          </div>
+          <div className="col">
+            <Card/>
+          </div>
+          <div className="col">
+            <Card/>
+          </div>
+        </div>
+      </div>
+
       <Boton />
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
     </>
   )
 }
